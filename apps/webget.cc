877edaecd24f04d292dd0cd9a27116cde5f8d2b3
zianglei <zianglei@126.com>
2020-12-18 06:47:26 +0000

    finish lab0

--- apps/webget.cc
+++ apps/webget.cc
@@ -12,13 +12,22 @@ void get_URL(const string &host, const string &path) {
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
+	Address destAddr(host, "http");
+	TCPSocket sock;
+	sock.connect(destAddr);
 
+	sock.write("GET " + path + " HTTP/1.1\r\n");
+	sock.write("Host: " + host + "\r\n");
+	sock.write("Connection: close\r\n\r\n");
+	
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
-
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+	std::string res;
+	while((res = sock.read()) != "") {
+		cout << res;
+	}
+	sock.close();
 }
 
 int main(int argc, char *argv[]) {
